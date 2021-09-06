63ca084e5c56dc0f2dbf03e38085ef40eb48a525
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-09-06 17:00:47 +0100

    cleaned up

--- src/socket.ts
+++ src/socket.ts
@@ -7,20 +7,11 @@ const server = createServer(app)
 
 const io = new Server(server, { allowEIO3: true })
 
-io.engine.generateId = function (req: any) {
-  console.log(req)
-  return 1
-}
-
 // adding "event listeners"
 io.on("connection", (socket) => {
-  console.log(socket.id)
-
   socket.on("login", (id, groups) => {
-    // tell all the users im online
     socket.broadcast.emit("online", { id })
 
-    // connect to all the groups
     socket.join(groups)
   })
 
@@ -35,21 +26,6 @@ io.on("connection", (socket) => {
 
     socket.to(room).emit("message", newMessage)
   })
-  //   socket.on("login", ({ username, room }) => {
-  //     // shared.onlineUsers.push({ username, id: socket.id, room })
-
-  //     socket.join(room)
-  //     console.log(socket.rooms)
-
-  //     // Emits to everyone excluding this client
-  //     socket.broadcast.emit("newLogin")
-  //     socket.emit("loggedin")
-  //   })
-
-  // //   socket.on("disconnect", () => {
-  // //     //console.log("socket disconnected")
-  // //     shared.onlineUsers = shared.onlineUsers.filter((user) => user.id !== socket.id)
-  // //   })
 })
 
 export default server
